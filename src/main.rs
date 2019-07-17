5058c5408033f888e6156235b971498be3807f05
Vlad Pănăzan <brgdvz@gmail.com>
2019-07-17 13:40:31 +0300

    add tests

--- src/main.rs
+++ src/main.rs
@@ -237,21 +237,78 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_test() {
+    fn gitlab_test() {
         let gitlab_repo1 = Repo {
             host: Host::Gitlab("gitlab.com".to_string()),
-            project: "vladDotfiles".to_string(),
             owner: "psnszsn".to_string(),
+            project: "simpler-notes".to_string(),
         };
 
         assert_eq!(
-            parse("https://gitlab.com/psnszsn/vladDotfiles").unwrap(),
+            parse("https://gitlab.com/psnszsn/simpler-notes").unwrap(),
             gitlab_repo1
         );
         assert_eq!(
             download(gitlab_repo1, PathBuf::from("/tmp/tests")).unwrap(),
             ()
         );
-        // assert_eq!(parse("gitlab:psnszsn/vladDotfiles").unwrap(), gitlab_repo1);
     }
+    #[test]
+    fn gitlabshort_test() {
+        let repo = Repo {
+            host: Host::Gitlab("gitlab.com".to_string()),
+            owner: "Rich-Harris".to_string(),
+            project: "degit-test-repo".to_string(),
+        };
+
+        assert_eq!(
+            parse("gitlab:Rich-Harris/degit-test-repo").unwrap(),
+            repo
+        );
+        assert_eq!(
+            download(repo, PathBuf::from("/tmp/tests")).unwrap(),
+            ()
+        );
+    }
+    #[test]
+    fn github_short() {
+        let repo = Repo {
+            host: Host::Github,
+            owner: "sveltejs".to_string(),
+            project: "template".to_string(),
+        };
+
+        assert_eq!(
+            parse("sveltejs/template").unwrap(),
+            repo
+        );
+        assert_eq!(
+            download(repo, PathBuf::from("/tmp/tests")).unwrap(),
+            ()
+        );
+    }
+  #[test]
+    fn gitlab_hosted() {
+        let repo = Repo {
+            host: Host::Gitlab("gitlab.gnome.org".to_string()),
+            owner: "bilelmoussaoui".to_string(),
+            project: "gtk-rust-template".to_string(),
+        };
+
+        assert_eq!(
+            parse("https://gitlab.gnome.org/bilelmoussaoui/gtk-rust-template").unwrap(),
+            repo
+        );
+        assert_eq!(
+            parse("git@gitlab.gnome.org:bilelmoussaoui/gtk-rust-template.git").unwrap(),
+            repo
+        );
+        assert_eq!(
+            download(repo, PathBuf::from("/tmp/tests")).unwrap(),
+            ()
+        );
+    }
+
+
+
 }
